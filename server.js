4c1c0d43e8ad4e60dc3956a501fb4669bee1a733
jeroenlooijtu <jeroen.looij2406@gmail.com>
2021-04-19 12:50:15 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const host = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) =>{
+    let result = {
+        'response': 'Miya atsumus greatest gift wasn\'t his talent or his love for volleyball, it was actually his twin brother Atsumu',
+        'status': 'Because i\'m a setter'
+    }
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(result));
+});
+
+server.listen(port, host, () => {
+    console.log(`server running at http://${host}:${port}/`)
+})
