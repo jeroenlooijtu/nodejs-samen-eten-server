a3de912b56ea6e49f4302fdaaf1b0ec927ea19f5
jeroenlooijtu <jeroen.looij2406@gmail.com>
2021-04-19 14:10:02 +0200

    changed from node to express

--- server.js
+++ server.js
@@ -6,13 +6,15 @@ const importData = require("./data.json")
 // const host = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) =>{
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(importData);
+app.get("/", (req, res) => {
+    res.send("Hello world XD");
 });
 
-server.listen(port, () => {
-    console.log(`server running at http://:${port}/`)
+app.get("/players", (req, res) => {
+    res.send(importData);
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port http://HaHAHAHA:${port}`);
 })
