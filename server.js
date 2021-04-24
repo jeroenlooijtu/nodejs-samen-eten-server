48d0dde0850a5ff4a9ca936a4eb5ac4ebd6040d6
survivorbat <djbatcat@gmail.com>
2021-04-24 14:11:41 +0200

    Add some formatting

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-
 const express = require("express");
-
 const app = express();
-const bodyParserx = require("body-parser");
+
+const bodyParser = require("body-parser");
+
 const port = process.env.PORT || 3000;
-app.use(bodyParserx.json());
+
+app.use(bodyParser.json());
+
 require("./app/routes/studenthomes.routes.js")(app);
 
-var server = app.listen(port, () => {
+const server = app.listen(port, () => {
   const host = server.address().address;
-  const { port } = server.address();
-  console.log("App listening at http://%s:%s", host, port);
+  const {port} = server.address();
+  console.log(`App listening at http://${host}:${port}`);
 });
