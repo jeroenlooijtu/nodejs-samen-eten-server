7d1502cfaf6378a0a1b4971c3135c82c73cbe902
jeroenlooijtu <jeroen.looij2406@gmail.com>
2021-04-24 14:05:01 +0200

    made port work

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require("express");
 
 const app = express();
 const bodyParserx = require("body-parser");
-
+const port = process.env.PORT || 3000;
 app.use(bodyParserx.json());
 require("./app/routes/studenthomes.routes.js")(app);
 
-var server = app.listen(3000, () => {
+var server = app.listen(port, () => {
   const host = server.address().address;
   const { port } = server.address();
   console.log("App listening at http://%s:%s", host, port);
